15e4ea0fa81c9d87199a70ec75a6f7af36c21c3e
diego <diegoasp@gmail.com>
2018-07-09 23:33:04 -0300

    Config UICollectionView of last games

--- Twitch.Games/Twitch.Games/Controllers/GamesViewController.swift
+++ Twitch.Games/Twitch.Games/Controllers/GamesViewController.swift
@@ -1,9 +1,32 @@
 import UIKit
 
-class GamesViewController: UIViewController {
+class GamesViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
 
+    //MARK: Data
+    var topGames: TopGames?;
+    var games: [Games] = [];    
+    
+    //MARK: Property
+    
+    //MARK: IBOutlet
+    @IBOutlet weak var collectionViewTopGames: UICollectionView!    
+    
+    //MARK: Lifecicle
     override func viewDidLoad() {
         super.viewDidLoad()
         logoInNavigation();
+        
+        self.collectionViewTopGames.delegate = self;
+        self.collectionViewTopGames.dataSource = self;        
     }
+    
+    //MARK: Delegate UICollectionView
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: " ", for: indexPath);
+        return cell;
+    }    
 }
